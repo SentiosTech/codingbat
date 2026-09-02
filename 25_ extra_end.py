f9d49ff4214a524ad38763900c0b2f336a2cfd4e
hitman <programmer_dev@outlook.com>
2026-09-01 22:38:40 -0600

    twenty fifth excercise

---
+++ 25_ extra_end.py
@@ -0,0 +1,27 @@
+"""
+Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+
+extra_end('Hello') → 'lololo'
+extra_end('ab') → 'ababab'
+extra_end('Hi') → 'HiHiHi'
+"""
+
+
+# normal
+def extra_end(str):
+    last_two = str[-2:]
+    return last_two * 3
+
+
+# optimal
+def extra_end(str):
+    return str[:-2] * 3
+
+
+# test
+if __name__ == "__main__":
+    extra_end("Hello") == "lololo"
+extra_end("ab") == "ababab"
+extra_end("Hi") == "HiHiHi"
+extra_end("Candy") == "dydydy"
+extra_end("Code") == "dedede"
