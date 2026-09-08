6138d887a6dfbdbf797c51e09c7aafa6f53b1aff
hitman <programmer_dev@outlook.com>
2026-09-08 11:08:35 -0600

    thirty first excersice

---
+++ 31_left2.py
@@ -0,0 +1,22 @@
+"""Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+
+left2('Hello') → 'lloHe'
+left2('java') → 'vaja'
+left2('Hi') → 'Hi'"""
+
+
+# optimal
+def left2(str):
+    return str[2:] + str[:2]
+
+
+# test
+if __name__ == "__main__":
+    left2("Hello") == "lloHe"
+    left2("java") == "vaja"
+    left2("Hi") == "Hi"
+    left2("code") == "deco"
+    left2("cat") == "tca"
+    left2("12345") == "34512"
+    left2("Chocolate") == "ocolateCh"
+    left2("bricks") == "icksbr"
