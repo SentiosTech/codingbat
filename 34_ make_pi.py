9d3cb1aa870b8b6e235f65f16ede8b3dc8e28a84
hitman <programmer_dev@outlook.com>
2026-09-16 13:27:27 -0600

    thirty fourth excersice

---
+++ 34_ make_pi.py
@@ -0,0 +1,15 @@
+"""
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+make_pi() → [3, 1, 4]
+"""
+
+
+# optimal
+def make_pi():
+    return [3, 1, 4]
+
+
+# test
+if __name__ == "__main__":
+    print(make_pi() == [3, 1, 4])
